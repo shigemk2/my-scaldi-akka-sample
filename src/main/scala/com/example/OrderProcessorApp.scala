fd786268484b3b2acfcf3a105f082a07cfa34fdc
shigemk2 <i.am.shige@gmail.com>
2016-08-10 23:31:23 +0900

    Rename OrderProcessorApp to AkkaModule

--- src/main/scala/com/example/OrderProcessorApp.scala
+++ src/main/scala/com/example/OrderProcessorApp.scala
@@ -5,6 +5,6 @@ class OrderModule extends Module {
   binding toProvider new OrderProcessor
   binding toProvider new PriceCalculator
 }
-object OrderProcessorApp extends Module {
+object AkkaModule extends Module {
     bind [ActorSystem] to ActorSystem("ScaldiModule") destroyWith (_.shutdown())
 }
