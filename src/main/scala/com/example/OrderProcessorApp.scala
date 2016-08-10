d9294a8eb253f52fcd4260cbfc1480e171d1d379
shigemk2 <i.am.shige@gmail.com>
2016-08-10 23:35:08 +0900

    Add OrderProcessorApp

--- src/main/scala/com/example/OrderProcessorApp.scala
+++ src/main/scala/com/example/OrderProcessorApp.scala
@@ -5,6 +5,29 @@ class OrderModule extends Module {
   binding toProvider new OrderProcessor
   binding toProvider new PriceCalculator
 }
+
 object AkkaModule extends Module {
     bind [ActorSystem] to ActorSystem("ScaldiModule") destroyWith (_.shutdown())
+}
+
+object OrderProcessorApp extends Module {
+  import Messages._
+
+  val userModule = new Module {
+    bind [UserService] to new SimpleUserService
+  }
+  implicit val appModule = new OrderModule :: userModule :: new AkkaModule
+
+  implicit val system = inject [ActorSystem]
+
+  val receptionist = injectActorRef [Receptionist]
+  val inbox = Inbox.create(system)
+
+  inbox send (receptionist, PriceOrder("test", 1, 1234))
+
+  val OrderProcessingStarted(_) = inbox.receive(5 seconds)
+  val OrderProcessed(orderNumber, grossAmount) = inbox.receive(5 seconds)
+
+  println(s"Order created. $orderNumber, Gross Amount: $grossAmount")
+  appModule.destroy
 }
