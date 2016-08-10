41331d2e57989611cde592b8c89da1abdda9439b
shigemk2 <i.am.shige@gmail.com>
2016-08-10 23:29:57 +0900

    Add OrderModule

--- src/main/scala/com/example/OrderProcessorApp.scala
+++ src/main/scala/com/example/OrderProcessorApp.scala
@@ -1,5 +1,10 @@
 package com.example
 
+class OrderModule extends Module {
+  binding toProvider new Receptionist
+  binding toProvider new OrderProcessor
+  binding toProvider new PriceCalculator
+}
 object OrderProcessorApp extends Module {
     bind [ActorSystem] to ActorSystem("ScaldiModule") destroyWith (_.shutdown())
 }
