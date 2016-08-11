7ad4f85cf6c66bf611ffcc974b372935cc9b21b1
shigemk2 <i.am.shige@gmail.com>
2016-08-12 00:08:48 +0900

    Add object Messages

--- src/main/scala/com/example/Receptionist.scala
+++ src/main/scala/com/example/Receptionist.scala
@@ -1,5 +1,16 @@
 package com.example
 
 class Receptionist {
+}
 
+object Messages {
+  case class PlaceOrder(userName: String, itemId: Long, netAmount: Int)
+  case class ProcessOrder(user: User, itemId: Long, netAmount: Int)
+  case class CalculatePrice(netAmount: Int)
+  case object CancelProcessing
+
+  case class OrderProcessingFailed(reason: String)
+  case class OrderProcessed(orderNumber: String, grossAmount: Int)
+  case class OrderProcessingStarted(processor: ActorRef)
+  case class GrossPriceCalculated(netPrice: Int, grossPrice: Int)
 }
